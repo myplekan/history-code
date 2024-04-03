6b8279ae645f3c678efa8fef29cec0843f53596f
Mykola Plekan <zikzar@gmail.com>
2024-04-03 13:37:24 +0300

    loader change

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -2,9 +2,9 @@ export const Loader = () => {
   return (
     <main className="flex items-center justify-center pt-11 h-screen">
       <img
-        src={process.env.PUBLIC_URL + "/images/svg/loader.svg"}
+        src={process.env.PUBLIC_URL + "/images/svg/loader.png"}
         alt="print"
-        className="w-32 animate-spin animate-thrice "
+        className="w-32 animate-spin"
       />
     </main>
   );
